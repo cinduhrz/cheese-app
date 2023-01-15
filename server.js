45cd42fccce94348b37463ed901e120b62c31bab
Cindy Wong <cindywongdev@gmail.com>
2023-01-15 11:28:21 -0500

    initial commit, set up server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+// IMPORT DEPENDENCIES
+require("dotenv").config() // load env variables
+const { PORT, DATABASE_URL } = process.env // destructure variables
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const morgan = require("morgan")
+
+
+// CREATE EXPRESS APP OBJECT
+const app = express()
+
+
+// MIDDLEWARE
+app.use(cors())
+app.use(morgan("dev"))
+
+
+// ROUTES
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+
+// TURN ON SERVER
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
